fbc8b1dedae3d9685a1388073b5c8eda8fce5413
Aria Elkaim <ariari.elk@gmail.com>
2022-08-10 16:34:02 -0400

    Merge function addToGrid into drawGrid()

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-function addToGrid(){
-    let gridBox = document.createElement('div');
-    gridBox.setAttribute('class', 'gridbox'); // set created div to class gridbox
-    document.getElementById('grid').appendChild(gridBox);
-}
-
 function drawGrid(){
     for (let i = 0; i < 257; i++){
-        addToGrid();
+        let gridBox = document.createElement('div');
+        gridBox.setAttribute('class', 'gridbox'); // set created div to class gridbox
+        document.getElementById('grid').appendChild(gridBox);
     }
 }
