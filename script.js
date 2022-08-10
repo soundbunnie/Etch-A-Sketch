b918dbe9c78796c6ebcc50a922e484b4bb1a8fcc
Aria Elkaim <ariari.elk@gmail.com>
2022-08-10 15:53:41 -0400

    Change gridBox variable type from var to let

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function addToGrid(){
-    var gridBox = document.createElement('div');
+    let gridBox = document.createElement('div');
     gridBox.setAttribute('class', 'gridbox'); // set created div to class gridbox
     document.getElementById('grid').appendChild(gridBox);
+}
+
+function drawGrid(){
+    for (let i = 0; i < 257; i++){
+        addToGrid();
+    }
 }
