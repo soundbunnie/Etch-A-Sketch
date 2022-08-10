17fd4263afc1d58fb7a3982a9c96e7c22252277f
Aria Elkaim <ariari.elk@gmail.com>
2022-08-10 17:07:53 -0400

    Add flex: 1 1 auto to gridbox so that it grows to fill row

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function drawGrid(){
-    for (let i = 0; i < 257; i++){
+    for (let i = 0; i < 16; i++){
         let gridBox = document.createElement('div');
         gridBox.setAttribute('class', 'gridbox'); // set created div to class gridbox
         document.getElementById('grid').appendChild(gridBox);
