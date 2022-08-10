7694cba5c94a25c5d30bfc6450d707651b0e5a46
Aria Elkaim <ariari.elk@gmail.com>
2022-08-10 19:01:12 -0400

    Make drawGrid take size variable with default value of 16

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function drawGrid(){
-    for (let i = 0; i < 16; i++){
+function drawGrid(size=16){
+    for (let i = 0; i < size; i++){
         let gridBox = document.createElement('div');
         gridBox.setAttribute('class', 'gridbox'); // set created div to class gridbox
         document.getElementById('grid').appendChild(gridBox);
