d680e5727426abd59e005f8c0e9980de7e20fdde
Aria Elkaim <ariari.elk@gmail.com>
2022-08-10 20:38:39 -0400

    Assign unique ID to each row and gridBox

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
+const gridBox = document.querySelectorAll('.gridbox')
+var rowIndex = 0;
+
 function drawGrid(size=16){
     let rows = document.getElementsByClassName('row');
     deleteGrid()
     for (let i = 0; i < size; i++){ // create x amount of rows
         let newRow = document.createElement('div');
         newRow.setAttribute('class', 'row');
+        newRow.setAttribute('id', `row-${i}`); //set id to row number
         document.getElementById('grid').appendChild(newRow); //append each row to grid
     }
     for (let row of rows){
         for (let i =0; i < size; i++){ //create x amount of boxes for each row
             let gridBox = document.createElement('div');
             gridBox.setAttribute('class', 'gridBox');
+            gridBox.setAttribute('id', `${rowIndex}-${i}`); //set id to rownumber - boxnumber
             row.appendChild(gridBox);
-        }      
+        }
+        rowIndex+=1;
     }
-    gridDrawn = true;
 }
-
 function deleteGrid(){
     let rows = document.querySelectorAll('.row');
-    let gridBox = document.querySelectorAll('.gridBox');
     rows.forEach(row =>{
         row.remove();
     })
@@ -27,4 +30,4 @@ function deleteGrid(){
     })
 }
 
-drawGrid()
+drawGrid();
